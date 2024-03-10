7fd3777b163f7d6a6ef8a43ea9fcc56db8e1c636
ppenter <jarupak.sri@gmail.com>
2024-03-10 17:46:16 +0700

    initialized

---
+++ app.js
@@ -0,0 +1,12 @@
+
+    module.exports = {
+      "name": "calculator",
+      "version": "0.0.1",
+      "rewrites": [
+        {
+          "from": "/calculator",
+          "to": "/calculator/pages/calculator"
+        }
+      ]
+    }
+    
---
+++ pages/landing/page.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+export default function Page() {
+  return <div className="w-screen h-screen flex flex-col justify-center items-center">
+  <h1 className="text-4xl font-bold">Hello</h1>
+  <p className="text-xl mt-4">Greeting from calculator</p>
+  <p className="mt-4">
+  Edit this file at src/apps/calculator/pages/landing/page.tsx
+  </p>
+</div>;
+}
+  
